f253852b8cbeaa32cf8633ccb3627cfeb09d9f46
Pierre Fernique <pfernique@gmail.com>
2017-02-13 14:13:29 +0100

    Update recipes

--- conda/python-statiskit_stl/bld.bat
+++ conda/python-statiskit_stl/bld.bat
@@ -1,6 +1,6 @@
 echo ON
 
-scons py --prefix=%LIBRARY_PREFIX% -j%CPU_COUNT% --arch=%ARCH%
+scons py --prefix=%LIBRARY_PREFIX% -j%CPU_COUNT% --arch=%ARCH% --msvc-version=14.0
 if errorlevel neq 0 exit 1
 
 echo OFF
