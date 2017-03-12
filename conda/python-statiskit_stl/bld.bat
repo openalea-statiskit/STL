24f3519bd44eef0802549cc5d981bc01d98b45eb
Pierre Fernique <pfernique@gmail.com>
2017-03-12 14:26:55 +0100

    Update bld.bat

--- conda/python-statiskit_stl/bld.bat
+++ conda/python-statiskit_stl/bld.bat
@@ -1,7 +1,7 @@
 echo ON
 
 set SCONS_MSCOMMON_DEBUG=-
-scons py --prefix=%LIBRARY_PREFIX% -j%CPU_COUNT% --arch=%ARCH% --msvc-version=14.0
+scons py --prefix=%LIBRARY_PREFIX% -j%CPU_COUNT% --arch=%ARCH%
 if errorlevel neq 0 exit 1
 
 echo OFF
