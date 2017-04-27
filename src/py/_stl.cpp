298b66411b37a98b3958306e1ecda9b40d736739
Pierre Fernique <pfernique@gmail.com>
2017-04-27 14:37:03 +0200

    Add Sublime Text project and unordered_set

--- src/py/_stl.cpp
+++ src/py/_stl.cpp
@@ -1,6 +1,7 @@
 #include "_stl.h"
 
 void wrapper_10b14312eeb655268489cd34090870cf();
+void wrapper_3b59a0980c80518c808634f7a84dc3cd();
 void wrapper_448c20257e485acda59dc59305fceb58();
 void wrapper_476c1c1f206251dba7af53c48f3f6e42();
 void wrapper_6436891c9b6854f494789a812891cbe5();
@@ -8,6 +9,7 @@ void wrapper_745e4933f5b250d9bc5232fe864d0cf7();
 void wrapper_74bc331707c1505eb2f807cae7c32bbb();
 void wrapper_858bbf0613575e2ebe4b0e5902107ad6();
 void wrapper_882a8dd1e64a51b4a9da29ab852a852e();
+void wrapper_d48105936d4f5d09a78d2aa9f878fdb8();
 void wrapper_e76a1308464d5a9a837d9a61f942fae7();
 void wrapper_f1ab29faa1815285a06ca52391d1425f();
 void wrapper_107131f9768c56e794a9b0de728d1738();
@@ -20,6 +22,7 @@ boost::python::docstring_options docstring_options(1, 0, 0);
 BOOST_PYTHON_MODULE(__stl)
 {
     wrapper_10b14312eeb655268489cd34090870cf();
+    wrapper_3b59a0980c80518c808634f7a84dc3cd();
     wrapper_448c20257e485acda59dc59305fceb58();
     wrapper_476c1c1f206251dba7af53c48f3f6e42();
     wrapper_6436891c9b6854f494789a812891cbe5();
@@ -27,6 +30,7 @@ BOOST_PYTHON_MODULE(__stl)
     wrapper_74bc331707c1505eb2f807cae7c32bbb();
     wrapper_858bbf0613575e2ebe4b0e5902107ad6();
     wrapper_882a8dd1e64a51b4a9da29ab852a852e();
+    wrapper_d48105936d4f5d09a78d2aa9f878fdb8();
     wrapper_e76a1308464d5a9a837d9a61f942fae7();
     wrapper_f1ab29faa1815285a06ca52391d1425f();
     wrapper_107131f9768c56e794a9b0de728d1738();
