816ab05d929e9043b8dca695d8a4dd3fcb1b7e04
Pierre Fernique <pfernique@gmail.com>
2017-07-27 12:39:48 +0200

    Test PROCESSOR_ARCHITECTURE

--- bin/conda/libstatiskit_stl/bld.bat
+++ bin/conda/libstatiskit_stl/bld.bat
@@ -1,10 +1,11 @@
 echo ON
 
 if "%ARCH%" == "32" (
-    set HOST_ARCH="x86"
+    set PROCESSOR_ARCHITECTURE="x86"
 ) else (
-    set HOST_ARCH="x64"
+    set PROCESSOR_ARCHITECTURE="x64"
 )
+
 
 scons cpp --prefix=%LIBRARY_PREFIX% -j%CPU_COUNT% --arch=%ARCH%
 if errorlevel 1 exit 1
