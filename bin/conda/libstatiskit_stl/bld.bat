23d36f72939a86b51c5df7f4c22d6842e939af46
Pierre Fernique <pfernique@gmail.com>
2017-07-27 12:32:08 +0200

    Test HOST_ARCH

--- bin/conda/libstatiskit_stl/bld.bat
+++ bin/conda/libstatiskit_stl/bld.bat
@@ -1,5 +1,11 @@
 echo ON
 
+if "%ARCH%" == "32" (
+    set HOST_ARCH="x86"
+) else (
+    set HOST_ARCH="x64"
+)
+
 scons cpp --prefix=%LIBRARY_PREFIX% -j%CPU_COUNT% --arch=%ARCH%
 if errorlevel 1 exit 1
 
