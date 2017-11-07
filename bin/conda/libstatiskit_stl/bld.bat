ca8d7d4fed7f21c49ce97a53a6fee55d864208c7
Pierre Fernique <pfernique@gmail.com>
2017-11-07 08:33:38 +0100

    Update bld.bat

--- bin/conda/libstatiskit_stl/bld.bat
+++ bin/conda/libstatiskit_stl/bld.bat
@@ -1,13 +1,6 @@
 echo ON
 
-if "%ARCH%" == "32" (
-    set PROCESSOR_ARCHITECTURE="x86"
-) else (
-    set PROCESSOR_ARCHITECTURE="x64"
-)
-set SCONS_MSCOMMON_DEBUG=-
-
-scons cpp --prefix=%LIBRARY_PREFIX% -j%CPU_COUNT% --arch=%ARCH%
+scons cpp --prefix=%LIBRARY_PREFIX% -j%CPU_COUNT%
 if errorlevel 1 exit 1
 
 echo OFF
