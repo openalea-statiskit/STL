491d083a8dd9e7397b74c2b9745692434262dc30
Pierre Fernique <pfernique@gmail.com>
2018-03-26 11:26:46 +0200

    Update

--- bin/conda/libstatiskit_stl-dev/build.sh
+++ bin/conda/libstatiskit_stl-dev/build.sh
@@ -1,6 +1,11 @@
 set -ve
 
-echo $SRC_DIR
-scons autowig --prefix=$PREFIX -j$CPU_COUNT --autowig-no-wrappers
+if [ "$(uname)" == "Darwin" ]; then
+   scons cpp-dev --prefix=$PREFIX -j$CPU_COUNT
+fi
+
+if [ "$(uname)" == "Linux" ]; then
+    scons autowig --prefix=$PREFIX -j$CPU_COUNT --autowig-no-wrappers
+fi
 
 set +ve
