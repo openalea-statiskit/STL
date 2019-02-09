116de42c65ea283191c7f7dd7f434aee181fa90f
Pierre Fernique <pfernique@gmail.com>
2019-02-09 09:24:46 +0100

    Switch to PyBind11

--- src/py/wrapper/wrapper_8f249c2863945cb59771b97c53bdafec.cpp
+++ src/py/wrapper/wrapper_8f249c2863945cb59771b97c53bdafec.cpp
@@ -8,7 +8,7 @@ void wrapper_8f249c2863945cb59771b97c53bdafec(pybind11::module& module)
 {
 
     pybind11::class_<struct ::std::binary_function< unsigned long int, unsigned long int, bool >, autowig::HolderType< struct ::std::binary_function< unsigned long int, unsigned long int, bool > >::Type > class_8f249c2863945cb59771b97c53bdafec(module, "_BinaryFunction_8f249c2863945cb59771b97c53bdafec", "");
-    class_8f249c2863945cb59771b97c53bdafec.def(pybind11::init<  >());
     class_8f249c2863945cb59771b97c53bdafec.def(pybind11::init< struct ::std::binary_function< unsigned long int, unsigned long int, bool > const & >());
+    class_8f249c2863945cb59771b97c53bdafec.def(pybind11::init<  >());
 
 }
